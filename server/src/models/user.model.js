61067ae2736e9fcca8cb7c2ff864206390a81b68
KAPIL DAHIYA <kapildahiya309@gmail.com>
2024-09-10 12:25:32 +0530

    Added user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  //    email, password, name, lastLogin, isVarified, resetPasswordToken, resetPasswordExpiresAt, varificationToken, varificationTokenExpiresAt
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowerCase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVarified: {
+      type: Date,
+      default: false,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpiresAt: {
+      type: Date,
+    },
+    varificationToken: {
+      type: String,
+    },
+    varificationTokenExpiresAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
