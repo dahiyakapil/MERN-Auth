87e340856b087c637e51df348370a067ff65a37e
KAPIL DAHIYA <kapildahiya309@gmail.com>
2024-09-10 12:04:11 +0530

    Added Production graded files

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import express from "express";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config({
+    path:"./.env"
+})
+
+
+const app = express();
+const PORT = 3000;
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Hello World !");
+});
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on ----> http://localhost:${PORT}`);
+})
+
